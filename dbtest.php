89c96cc8461f5ed0cdff1ae1291ca3517228e21b
lmredhat <lmartinh@redhat.com>
2019-03-04 12:52:52 +0100

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -16,6 +16,4 @@ $rs = $connection->query($query);
 while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
     echo $row['user_id'] . " " . $row['username'] . "\n";
 }
-$rs->close();
-$connection->close();
 ?>
