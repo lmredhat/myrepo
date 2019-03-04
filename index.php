280b6cb962db3fbafd17bdee74414c4faf832e68
lmredhat <lmartinh@redhat.com>
2019-03-04 14:42:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 echo "This is a simple php application";
 echo "<br>Welcome to lilly.  Grant is the worst";
 
+$x = 0; 
+
+while($x <= 5) {
+    echo "<br>The number is: $x <br>";
+    $x++;
+    sleep(2);
