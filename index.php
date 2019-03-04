99d350acc0a4fa9228bff6c4a71e583fc351c508
lmredhat <lmartinh@redhat.com>
2019-03-04 15:09:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,20 @@
 echo "This is a simple php application";
 echo "<br>Welcome to lilly.  Grant is the worst";
 
-sleep(2);
-
+sleep(4);
 echo "<br>Welcome to TONILANDIA.";
+
+sleep(4);
+echo "<br>2345678987654323456789.";
+
+sleep(4);
+echo "<br>OEO.";
+
+sleep(4);
+echo "<br>Welcome to TONILANDIA.";
+
+sleep(4);
+echo "<br>2345678987654323456789.";
+
+sleep(4);
+echo "<br>OEO.";
